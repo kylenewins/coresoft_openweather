e5f0c781eff22e7700b7a1e99a90e03ae956a505
Toad Sage <kylenewins@gmail.com>
2019-05-03 12:44:35 -0400

    ajax formatting start

--- public/js/fiveDay.js
+++ public/js/fiveDay.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
     console.log("fiveDay.js linked")
+
+    // const axios = require('axios')
+
+    const API_KEY = "9a0bdff0024086732f9ae7a001e24a22"
+    const queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Belmar,us&mode=json&appid=" + API_KEY
+
+    $.ajax({
+        URL: queryUrl,
+        METHOD: "GET"
+    }).then(function(res){
+        console.log(queryUrl)
+    })
+    
 })
