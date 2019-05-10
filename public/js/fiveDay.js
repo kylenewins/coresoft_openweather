8c2faec3771c97e494f60022d5a3ad30d7ed38ef
Toad Sage <kylenewins@gmail.com>
2019-05-10 12:02:09 -0400

    basic five day call

--- public/js/fiveDay.js
+++ public/js/fiveDay.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
     // const axios = require('axios')
 
     const API_KEY = "9a0bdff0024086732f9ae7a001e24a22"
-    const queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=Belmar,us&mode=json&appid=" + API_KEY
-    const data = {}
+    const queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=asbury%20park,us&mode=json&appid=" + API_KEY + "&units=imperial"
+    const data = []
 
 
     $.get(queryUrl).then(function(result){
@@ -15,9 +15,32 @@ $(document).ready(function(){
             var day = $("<div></div>")
             day.attr("id", "dayDiv")
 
-            var temp = result.list[i].main.temp
-            var desc = result.list[i].weather.main
+            var temp = $("<h6></h6>")
+            temp.text("Low: " + result.list[i].main.temp_min + " High: " + result.list[i].main.temp_max)
 
+            var desc = $("<h6></h6>")
+            desc.text(result.list[i].weather[0].description)
+
+            var iconcode = result.list[i].weather[0].icon
+            var iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+            var icon = $("<img></img")
+            icon.attr("class", "center-block")
+            icon.attr("src", iconurl)
+            icon.attr("alt", "Weather Icon")
+
+            var singleData = {
+                temperature: temp,
+                description: desc,
+                icon: iconcode
+            }
+
+            data.push(singleData)
+
+            desc.appendTo(day)
+            icon.appendTo(day)
+            temp.appendTo(day)
+
+            day.appendTo($("#fiveDayData"))
         }
 
     })
