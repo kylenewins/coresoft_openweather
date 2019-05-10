332c0a57d7fefafd2d7c61f9b25463647fb63860
Toad Sage <kylenewins@gmail.com>
2019-05-10 11:04:48 -0400

    API functionality started

--- public/js/fiveDay.js
+++ public/js/fiveDay.js
@@ -4,13 +4,28 @@ $(document).ready(function(){
     // const axios = require('axios')
 
     const API_KEY = "9a0bdff0024086732f9ae7a001e24a22"
-    const queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Belmar,us&mode=json&appid=" + API_KEY
+    const queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=Belmar,us&mode=json&appid=" + API_KEY
+    const data = {}
 
-    $.ajax({
-        URL: queryUrl,
-        METHOD: "GET"
-    }).then(function(res){
-        console.log(queryUrl)
+
+    $.get(queryUrl).then(function(result){
+        console.log(result)
+
+        for(var i = 0; i<5; i ++){
+            var day = $("<div></div>")
+            day.attr("id", "dayDiv")
+
+            var temp = result.list[i].main.temp
+            var desc = result.list[i].weather.main
+
+        }
+
     })
     
+
+    $("#saveFive").on("click", function(event){
+        event.preventDefault()
+
+    })
+   
 })
